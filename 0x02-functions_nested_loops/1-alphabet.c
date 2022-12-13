8ade3150229612a94b68a6aa350a0b181e231f6e
raskolinckov <ahmedoonny@gmail.com>
2022-12-13 00:41:44 -0800

    execute

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "putchar.h"
+/**
+ * print_alphabet - Print alphabet in lowercase using only "_putchar"
+ * Return: 0
+ */
+void print_alphabet(void),
+  
+{
+  char ch = 'a';
+  
+  while (ch <= 'z')
+    {
+      _putchar(ch);
+      ch++;
+    }
+  _putchar('\n');
+}
