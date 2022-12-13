6dbce41130f709e8ef85387382c7112d820abc2a
raskolinckov <103078933+raskolinckov@users.noreply.github.com>
2022-12-13 11:15:11 +0200

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -4,16 +4,20 @@
  * Description: Standard libraries are not allowed.
  * Return: 0
  */
+
 int main(void)
+
 {
 	char _putchar[] = "_putchar";
 	int i = 0;
 
 	while (_putchar] != '\0')
+
 	{
 		_putchar(_putchar[i]);
 		i++;
 	}
+
 	_putchar('\n');
 
 	return (0);
