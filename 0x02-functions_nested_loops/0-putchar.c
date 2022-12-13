dc3de0b3f9f0dc85b6844c13275e1ee1a3932766
raskolinckov <103078933+raskolinckov@users.noreply.github.com>
2022-12-13 10:12:54 +0200

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,15 +6,15 @@
  */
 int main(void)
 {
-  char _putchar[] = "_putchar";
-  int i = 0;
+	char _putchar[] = "_putchar";
+	int i = 0;
 
-  while (_putchar[i] != '\0')
-    {
-      _putchar(_putchar[i]);
-      i++;
-    }
-  _putchar('\n');
+	while (_putchar] != '\0')
+	{
+		_putchar(_putchar[i]);
+		i++;
+	}
+	_putchar('\n');
 
-  return (0);
+	return (0);
 }
