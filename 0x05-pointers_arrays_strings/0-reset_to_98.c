ef523e42ffc14b559fd03934558e49a05bc821c0
raskolinckov <103078933+raskolinckov@users.noreply.github.com>
2022-12-31 09:10:24 +0200

    Update 0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,1 +1,14 @@
-gjsj
+#include "main.h"
+/**
+ * reset_to_98 - takes the pointer and updates the value
+ *
+ * @n: int to check
+ *
+ * Return: 0 is success
+ */
+void reset_to_98(int *n)
+{
+
+	*n = 98;
+}
+Footer
