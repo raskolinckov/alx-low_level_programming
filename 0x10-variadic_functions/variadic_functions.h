fa287ce2f7779898388c2776c0e7fe1f1ef6031c
raskolinckov <103078933+raskolinckov@users.noreply.github.com>
2023-01-20 00:25:58 +0200

    Update variadic_functions.h

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,27 +1,20 @@
-#ifndef _VARIADIC_FUNCTIONS_H
-#define _VARIADIC_FUNCTIONS_H
+#ifndef VARIADIC_FUNCTIONS_H
+#define VARIADIC_FUNCTIONS_H
 #include <stdarg.h>
-#include <stdio.h>
 
-void printchar(va_list charlist);
-void printinteger(va_list charlist);
-void printfloat(va_list charlist);
-void printstring(va_list charlist);
-
-typedef struct characters
+/**
+ * struct printer - A new struct type defining a printer.
+ * @symbol: A symbol representing a data type.
+ * @print: A function pointer to a function that prints
+ *         a data type corresponding to symbol.
+ */
+typedef struct printer
 {
-        /* pointer to the arguments passed to the function print_all) */
-        char *arguments;
-        /* pointer to function print_all that returns nothing */
-        /* and receive a list of characters(arguments) */
-	/* va_list is a type that iterates arguments */
-	/* va_list as type of  variable we're going to name charlist */
-	/* so the pointer points a function that receive va_list) */
-        void (*ptrfunc)(va_list);
-} characters;
+	char *symbol;
+	void (*print)(va_list arg);
 
+} printer_t;
 
-int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
