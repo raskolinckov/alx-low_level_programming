7999ce58eb094960b5162dd7449e68fbbd2eddc9
raskolinckov <103078933+raskolinckov@users.noreply.github.com>
2023-01-25 10:15:10 +0200

    Update function_pointers.h

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,13 +1,16 @@
-#ifndef _FUNCTION_POINTERS_H
-#define _FUNCTION_POINTERS_H
-#include <stddef.h>
-/* stddef.h header defines various types and macros */
-/* size_t is unsigned integral type and is the result of the sizeof keyword */
+#ifndef FUNCTION_POINTERS_H
+#define FUNCTION_POINTERS_H
 
-int _putchar(char c);
+/*
+ * File: function_pointers.h
+ * Desc: Header file containing prototypes for all functions
+ *       used in the function_pointers directory.
+ */
+
+#include <stdlib.h>
+
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
 
 #endif
-Footer
