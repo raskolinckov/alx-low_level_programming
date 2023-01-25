fd3e55e2aa664a5df237af6ee023c4efddf1b325
raskolinckov <103078933+raskolinckov@users.noreply.github.com>
2023-01-25 10:13:44 +0200

    Update 0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,19 +1,14 @@
 #include "function_pointers.h"
+
 /**
- * print_name - prints a name
- *
- * @name: pointer to a char
- * @f: a pointer to function that receives a pointer to char
- *
- * Return: void or NULL
+ * print_name - Prints a name.
+ * @name: The name to be printed.
+ * @f: A pointer to a function that prints a name.
  */
 void print_name(char *name, void (*f)(char *))
 {
-	/* condition for the two pointers they must exist */
-	/* also could be if they are different to NULL */
-	if (name && f)
-		/*de reference the pointer to the function f */
-		/* to the pointer name */
-		/* f receives a char *, and *name is type char */
-		f(name);
+	if (name == NULL || f == NULL)
+		return;
+
+	f(name);
 }
